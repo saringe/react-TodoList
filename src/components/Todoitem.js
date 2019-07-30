ec79bb2b2d939588c9906798fbfb22ef9fb5bda5
Cliff Saringe <saringecliff@gmail.com>
2019-07-30 16:12:47 +0300

    Todo input logic

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react'
 
 export default class Todoitem extends Component {
     render() {
-        return (
-            <div>
-                <h2>from Todoitem</h2>
-            </div>
-        )
+        return <h1>hello</h1>
+            
+        
     }
 }
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
-import Item from './Todoitem'
+import Todoitem from './Todoitem'
 
 export default class Todolist extends Component {
     render() {
+        const {items, handleDelete,handleEdit,clearList}= this.props;
         return (
-            <div>
-                <h1>from Todolist</h1>
-                <Item></Item>
-            </div>
+         <ul className="list-group my-5 ">
+             <h3 className="text-capitalize text-center">todo list</h3>
+           {items.map(item => {
+                 return(
+                     <Todoitem
+                     key={item.id} 
+                     title={item.id} 
+                     handleDelete={()=>handleDelete(item.id) }
+                    handleEdit={()=>handleEdit(item.id)}
+                   ></Todoitem>
+                 )
+             })}
+                <button className="btn btn-danger btn-block text-center mt-5" onClick={clearList}>CLEAR LIST</button>
+         </ul>
         )
     }
 }
